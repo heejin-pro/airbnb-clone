09366bf2ecaa6f3dd4acc46c2e14ab050ce169de
박희진 <hjin.pro@gmail.com>
2020-11-12 23:30:16 +0900

    Edit Room Model and Create Review Model

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    update = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)  # auto_now_add : 모델이 생성된 날짜를 구함
+    update = models.DateTimeField(auto_now=True)  # auto_now : 새로운 날짜로 업데이트
 
     class Meta:
-        abstract = True
+        abstract = True  # Table을 만들지 않는 Model
