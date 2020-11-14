9a3d7aba12bf986699665734d65f734d9eaa1dc1
박희진 <hjin.pro@gmail.com>
2020-11-15 03:13:42 +0900

    create Custom manage.py command to Create Samle Data(2)

---
+++ rooms/management/commands/seed_rooms.py
@@ -0,0 +1,40 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from rooms import models as room_models
+from users import models as user_models
+
+
+class Command(BaseCommand):
+
+    help = "This Command is Create Many Sample Rooms"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=2,
+            type=int,
+            help="How many rooms do you want to create?",
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        all_user = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": seeder.faker.address(),
+                "host": lambda x: random.choice(all_user),
+                "room_type": lambda x: random.choice(room_types),
+                "guests": lambda x: random.randint(0, 10),
+                "price": lambda x: random.randint(0, 300),
+                "beds": lambda x: random.randint(0, 5),
+                "bedrooms": lambda x: random.randint(0, 5),
+                "baths": lambda x: random.randint(0, 5),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} Rooms created!"))
