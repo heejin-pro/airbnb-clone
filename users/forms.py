06a8215ab45967a0284e61baca6ab33697632e0f
박희진 <hjin.pro@gmail.com>
2020-11-22 21:54:42 +0900

    Add SignUp View with FormView

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from . import models
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
-    passsword = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -18,3 +18,40 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exsit"))
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_chk = forms.CharField(widget=forms.PasswordInput, label="confirm password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_passsword_chk(self):
+        password = self.cleaned_data.get("password")
+        password_chk = self.cleaned_data.get("passsword_chk")
+
+        if password != password_chk:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
