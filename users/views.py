5d70e294ca483451a5037f93d99ee45dc0d6576b
heejin park <hjin.pro@gmail.com>
2020-12-12 23:33:32 +0900

    Add User's Password Change View with PasswordChangeView

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ import os
 import requests
 from django.views import View
 from django.views.generic import FormView, DetailView, UpdateView
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -236,3 +237,7 @@ class UpdateProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UpdatePasswordView(PasswordChangeView):
+    template_name = "users/update_password.html"
