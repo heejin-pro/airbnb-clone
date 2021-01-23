dde98efe8184275290e3b73422256a03670898e7
heejin park <hjin.pro@gmail.com>
2021-01-23 21:46:50 +0900

    Extending manager of the model

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+from . import managers
 
 
 class BookedDay(core_models.TimeStampedModel):
@@ -41,6 +42,8 @@ class Reservation(core_models.TimeStampedModel):
     room = models.ForeignKey(
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
+
+    objects = managers.CustomReservationManager()
 
     def in_progress(self):
         now = timezone.now().date()
