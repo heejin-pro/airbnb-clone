c6e397a28d2a55bd7f48fe841a4cc5de47a3c26f
heejin park <hjin.pro@gmail.com>
2021-01-26 01:22:40 +0900

    Modify templatetags in rooms

--- rooms/templatetags/is_booked.py
+++ rooms/templatetags/is_booked.py
@@ -5,14 +5,13 @@ from reservations import models as reservation_models
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
+@register.simple_tag
 def is_booked(room, day):
     if day.number == 0:
         return
-    else:
-        try:
-            date = datetime.datetime(year=day.year, month=day.month, day=day.number)
-            reservation_models.BookedDay.objects.get(day=date, reservation__room=room)
-            return True
-        except reservation_models.BookedDay.DoesNotExist:
-            return False
+    try:
+        date = datetime.datetime(year=day.year, month=day.month, day=day.number)
+        reservation_models.BookedDay.objects.get(day=date, reservation__room=room)
+        return True
+    except reservation_models.BookedDay.DoesNotExist:
+        return False
