d9c3c393543b9aac16d40d5e06233326b95475d8
heejin park <hjin.pro@gmail.com>
2021-01-28 01:12:27 +0900

    Create page of user's favorite room list

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.views.generic import TemplateView
 from rooms import models as room_models
 from . import models
 
@@ -15,3 +16,7 @@ def toggle_room(request, room_pk):
         elif action == "remove":
             the_list.rooms.remove(room)
     return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
+
+
+class SeeFavView(TemplateView):
+    template_name = "lists/list_detail.html"
